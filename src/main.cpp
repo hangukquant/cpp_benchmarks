9ea33d0d89b023a4cd6d271efdcae23549b2d463
HangukQuant <97517358+hangukquant@users.noreply.github.com>
2025-06-13 14:59:14 +0800

    abstract

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,39 @@
 #include <boost/asio/ssl.hpp>
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/websocket/ssl.hpp>
 #include <iostream>
 #include <string>
 #include <fmt/core.h>
-#include <boost/beast/websocket/ssl.hpp>
 
 namespace beast = boost::beast;
 namespace websocket = beast::websocket;
 namespace net = boost::asio;
 namespace ssl = net::ssl;
 using tcp_socket = net::ip::tcp::socket;
+
+auto connect_websocket(
+    net::io_context& ioc,
+    ssl::context& ctx,
+    const std::string& host,
+    const std::string& port,
+    const std::string& target
+) {
+    net::ip::tcp::resolver resolver(ioc);
+    websocket::stream<ssl::stream<tcp_socket>> ws(ioc, ctx);
+
+    auto const results = resolver.resolve(host, port);
+    net::connect(ws.next_layer().next_layer(), results.begin(), results.end());
+    ws.next_layer().handshake(ssl::stream_base::client);
+
+    if(!SSL_set_tlsext_host_name(ws.next_layer().native_handle(), host.c_str())) {
+        beast::error_code ec{static_cast<int>(::ERR_get_error()), net::error::get_ssl_category()};
+        throw beast::system_error{ec};
+    }
+
+    ws.handshake(host, target);
+    return ws;
+}
 
 int main() {
     fmt::print("hi\n");
@@ -26,19 +49,7 @@ int main() {
         ssl::context ctx(ssl::context::tlsv12_client);
         ctx.set_default_verify_paths();
 
-        net::ip::tcp::resolver resolver(ioc);
-        websocket::stream<ssl::stream<tcp_socket>> ws(ioc, ctx);
-
-        auto const results = resolver.resolve(host, port);
-        net::connect(ws.next_layer().next_layer(), results.begin(), results.end());
-        ws.next_layer().handshake(ssl::stream_base::client);
-
-        if(!SSL_set_tlsext_host_name(ws.next_layer().native_handle(), host.c_str())) {
-            beast::error_code ec{static_cast<int>(::ERR_get_error()), net::error::get_ssl_category()};
-            throw beast::system_error{ec};
-        }
-
-        ws.handshake(host, target);
+        auto ws = connect_websocket(ioc, ctx, host, port, target);
 
         std::cout << "Connected to Binance BTCUSDT trade stream. Printing messages:\n";
         for (;;) {
